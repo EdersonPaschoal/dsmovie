b042b35df8d3dc4b7e46fdc4f1402bb215c670dd
EdersonPaschoal <ederson.paschoal@hotmail.com>
2022-03-26 11:10:33 -0300

    Show score

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -23,7 +23,7 @@ function Listing() {
     });
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12page=${pageNumber}&sort=title`)
+        axios.get(`${BASE_URL}/movies?size=12page=${pageNumber}&sort=Id`)
             .then(response => {
                 const data = response.data as MoviePage;
                 setPage(data);
